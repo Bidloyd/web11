7d2dfa7df5a5ac402e08cd7bdd14bac371f138f4
Bidloyd <nedoo228@gmail.com>
2022-12-08 15:47:01 +0300

    11

--- mntn/js/mobile-btn.js
+++ mntn/js/mobile-btn.js
@@ -1,7 +1,16 @@
 const navBtn = document.querySelector(".mobile__nav");
 const mobileNav = document.querySelector(".mobile__menu");
-
+const mobileNavActive = document.querySelector("mobile__menu-active");
+//Opening mmobile menu and switching mobile btn
 navBtn.addEventListener("click", function () {
   mobileNav.classList.toggle("mobile__menu-active");
   navBtn.classList.toggle("mobile__nav-close");
+});
+//Closing mobile menu when click is outside the div
+window.addEventListener("click", (e) => {
+  const target = e.target;
+  if (!target.closest(".mobile__menu") && !target.closest(".mobile__nav")) {
+    mobileNav.classList.remove("mobile__menu-active");
+    navBtn.classList.remove("mobile__nav-close");
+  }
 });
